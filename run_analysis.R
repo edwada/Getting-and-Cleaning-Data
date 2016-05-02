882b6dfce9a44984ec02ce0cd9e5f1954b96d43e
edwada <eduardo.wada@gmail.com>
2016-05-02 00:16:44 -0300

    Added comments

--- run_analysis.R
+++ run_analysis.R
@@ -16,12 +16,15 @@ subjects_train <- read.table("Data\\ProjectData\\UCI HAR Dataset\\train\\subject
 X_train <- read.table("Data\\ProjectData\\UCI HAR Dataset\\train\\X_train.txt")
 Y_train <- read.table("Data\\ProjectData\\UCI HAR Dataset\\train\\Y_train.txt")
 
+# 1. Merges the training and the test sets to create one data set.
 subjects_merged <- rbind(subjects_test, subjects_train)
 X_merged <- rbind(X_test, X_train) 
 Y_merged <- rbind(Y_test, Y_train) 
 
 
 features <- read.table("Data\\ProjectData\\UCI HAR Dataset\\features.txt")
+
+# 3. Uses descriptive activity names to name the activities in the data set
 activity_labels <- read.table("Data\\ProjectData\\UCI HAR Dataset\\activity_labels.txt")
 activity_factor <- factor(activity_labels[,2])
 
@@ -31,6 +34,8 @@ colnames(Y_merged) <- c("Activity")
 Y_merged <- mutate(Y_merged, Activity = activity_factor[Activity])
 
 names(X_merged) <- make.names(names=names(X_merged), unique=TRUE, allow_ = TRUE)
+
+# 2.Extracts only the measurements on the mean and standard deviation for each measurement.
 X_merged[, -grep("mean\\(\\)|std\\(\\)", colnames(df))]
 
 
@@ -38,6 +43,8 @@ X_merged[, -grep("mean\\(\\)|std\\(\\)", colnames(df))]
 
 tidyData <- cbind(subjects_merged, Y_merged, X_merged)
 
+# From the data set in step 4, creates a second, independent tidy data set with the average 
+# of each variable for each activity and each subject.
 result <- group_by(tidyData, Subject, Activity) %>% summarise_each(funs(mean))
 
 write.table(x = result, file = "result.txt", row.name = FALSE)
