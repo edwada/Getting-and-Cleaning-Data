c5a160521792a4e045b77136df80e3e03603e53a
Eduardo Wada <Eduardo Wada>
2017-07-30 19:43:45 +0100

    Fixed column cleaning

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,12 @@
 library(plyr)
 library(dplyr)
 
-download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
+if(!file.exists("Data//ProjectData.zip")){
+  download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
               destfile = "Data//ProjectData.zip")
 
-unzip(zipfile = "Data//ProjectData.zip", overwrite = TRUE, exdir = "Data//ProjectData")
-
+  unzip(zipfile = "Data//ProjectData.zip", overwrite = TRUE, exdir = "Data//ProjectData")
+}
 
 #loading test data
 subjects_test <- read.table("Data\\ProjectData\\UCI HAR Dataset\\test\\subject_test.txt")
@@ -28,15 +29,15 @@ features <- read.table("Data\\ProjectData\\UCI HAR Dataset\\features.txt")
 activity_labels <- read.table("Data\\ProjectData\\UCI HAR Dataset\\activity_labels.txt")
 activity_factor <- factor(activity_labels[,2])
 
-subjects_merged <- rename(subjects_merged, c("V1" = "Subject"))
+subjects_merged <- plyr::rename(subjects_merged, c("V1" = "Subject"))
 colnames(X_merged) <- features[,2]
 colnames(Y_merged) <- c("Activity")
 Y_merged <- mutate(Y_merged, Activity = activity_factor[Activity])
 
-names(X_merged) <- make.names(names=names(X_merged), unique=TRUE, allow_ = TRUE)
+#names(X_merged) <- make.names(names=names(X_merged), unique=TRUE, allow_ = TRUE)
 
 # 2.Extracts only the measurements on the mean and standard deviation for each measurement.
-X_merged[, -grep("mean\\(\\)|std\\(\\)", colnames(df))]
+X_merged <- X_merged[, grep(".*-mean|.*-std", colnames(X_merged))]
 
 
 
@@ -45,6 +46,9 @@ tidyData <- cbind(subjects_merged, Y_merged, X_merged)
 
 # From the data set in step 4, creates a second, independent tidy data set with the average 
 # of each variable for each activity and each subject.
-result <- group_by(tidyData, Subject, Activity) %>% summarise_each(funs(mean))
+beforeGroup <- tidyData
+afterGroup <- group_by(tidyData, Subject, Activity)
+afterSummarise <- afterGroup %>% summarise_each(funs(mean))
+result <- afterSummarise
 
 write.table(x = result, file = "result.txt", row.name = FALSE)
